1f97c402d4729e8908a9c997cdf3e1982b5d143b
jusruizsa <sruizs.jr@gmail.com>
2023-04-22 19:56:41 -0500

    app.py modified for only launch the app

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from flask import Flask
+from routes.routes import load_routes
 
 # App Config
 app = Flask(__name__)
-
-def load_routes(app):
-    from controller.personalController import get_personal
-    app.add_url_rule('/personal', view_func=get_personal)
-
 
 if __name__ == '__main__':
     load_routes(app)
