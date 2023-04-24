4cbd03cbbe74272da941495c2064155dff8020de
jusruizsa <sruizs.jr@gmail.com>
2023-04-24 16:20:49 -0500

    add flask config

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from routes.routes import load_routes
+from flask_cors import CORS
 
 # App Config
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+CORS(app)
 
 if __name__ == '__main__':
     load_routes(app)
     app.run(debug=True)
-
-
